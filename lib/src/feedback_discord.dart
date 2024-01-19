972c1305588458768636d590f32a6132517185e0
brasizza <mvbdesenvolvimento@gmail.com>
2024-01-18 23:32:07 -0300

    first commit

---
+++ lib/src/feedback_discord.dart
@@ -0,0 +1,48 @@
+import 'package:feedback/feedback.dart';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
+
+/// This is an extension to make it easier to call
+/// [showAndUploadToDiscord].
+extension BetterFeedbackDiscord on FeedbackController {
+  void showAndUploadToDiscord({
+    required String channel,
+    required String discordUrl,
+    http.Client? client,
+  }) {
+    show(uploadToDiscord(
+      channel: channel,
+      discordUrl: discordUrl,
+      client: client,
+    ));
+  }
+}
+
+/// See [BetterFeedbackX.showAndUploadToDiscord].
+/// This is just [visibleForTesting].
+@visibleForTesting
+OnFeedbackCallback uploadToDiscord({
+  required String channel,
+  required String discordUrl,
+  http.Client? client,
+}) {
+  final httpClient = client ?? http.Client();
+  final baseUrl = discordUrl;
+
+  return (UserFeedback feedback) async {
+    final uri = Uri.parse(
+      baseUrl,
+    );
+    final uploadRequest = http.MultipartRequest('POST', uri)
+      ..fields['content'] = feedback.text
+      ..files.add(http.MultipartFile.fromBytes(
+        'file',
+        feedback.screenshot,
+        filename: 'feedback.png',
+        contentType: MediaType('image', 'png'),
+      ));
+
+    await httpClient.send(uploadRequest);
+  };
+}
